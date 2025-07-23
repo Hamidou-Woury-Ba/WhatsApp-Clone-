479125e4b2034cea592c76cc0d5079469b0a5f74
Hamidou Woury Ba <hamidouwouryba13@gmail.com>
2025-07-23 13:57:41 +0000

    Sychronize the users with keycloak

---
+++ whatsappclone/src/main/java/com/hamidou/whatsappclone/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.hamidou.whatsappclone.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Query(name = UserConstants.FIND_USER_BY_EMAIL)
+    Optional<User> findByEmail(@Param("email") String userEmail);
+
+}
